889dbc6348cc49c1477957308d293499f6d5c457
gabrielmarcos2 <gabrielsqueda123@gmail.com>
2022-03-04 22:08:28 -0300

    finalizar musica

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿Console . ForegroundColor  =  ConsoleColor . DarkBlue ;
+Console.WriteLine("Peraí");
+Console.ResetColor();
+
+Console.WriteLine("Mas se você não sabe eu preciso te dizer");
+Console.WriteLine("É difícil ser criança, tanta coisa pra aprender");
+Console.WriteLine("Resolvi deixar a vida ela mesma me contar");
+Console.WriteLine("Ainda tenho tempo, pode crer deixa rolar");
+Console.WriteLine("");
+Console.WriteLine("Imagina nessa idade o que aconteceu comigo");
+Console.WriteLine("Resolvi me apaixonar, por alguém que não consigo");
+Console.WriteLine("Ironia do destino que só quer brincar com a gente");
+Console.WriteLine("Não sei lidar com isso, tá tudo diferente");
+Console.WriteLine("");
+
+Console . ForegroundColor  =  ConsoleColor . Red ;
+Console.WriteLine("Peraí, criança tem que ser feliz");
+Console.WriteLine("Peraí, é isso coração me diz");
+Console.WriteLine("Mais um sonho na cabeça");
+Console.WriteLine("Esperando que aconteça");
+Console.ResetColor();
+
+Console.WriteLine("");
+Console.WriteLine("Peraí, por isso eu venho aqui cantar");
+Console.WriteLine("Ouça aí, eu sei você quer escutar");
+Console.WriteLine("Tudo é possível é só acreditar");
+Console.WriteLine("");
+Console.WriteLine("O primeiro amor não se esquece facilmente");
+Console.WriteLine("Ainda mais quando esse amor");
+Console.WriteLine("Não quer nem saber da gente");
+Console.WriteLine("Meu pai diz que sou homem e não posso vacilar");
+Console.WriteLine("Mas ainda sou criança, tenho que desabafar");
+Console.WriteLine("");
+Console.WriteLine("Não consigo entender o que é que nos separa");
+Console.WriteLine("Quando vejo o teu rosto, meu coração dispara");
+Console.WriteLine("Mas arranjei um jeito de chamar tua atenção");
+Console.WriteLine("Peraí é pra você, que eu fiz essa canção");
+Console.WriteLine("");
+
+Console . ForegroundColor  =  ConsoleColor . Red ;
+Console.WriteLine("Peraí, criança tem que ser feliz");
+Console.WriteLine("Peraí, é isso coração me diz");
+Console.WriteLine("Mais um sonho na cabeça");
+Console.WriteLine("Esperando que aconteça");
+Console.ResetColor();
+
+Console.WriteLine("");
+Console.WriteLine("Peraí, por isso eu venho aqui cantar");
+Console.WriteLine("Ouça aí, eu sei você quer escutar");
+Console.WriteLine("Tudo é possível é só acreditar");
+Console.WriteLine("");
